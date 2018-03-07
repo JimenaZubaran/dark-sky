ef2ad68df4fc13a0672f95aebe41f0b72fd1ff26
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-03-07 13:35:47 -0600

    amalia

--- js/app.js
+++ js/app.js
@@ -1,66 +1,94 @@
-const form = $("#search-form");
+const firsAjax = function(){
+  let url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/f3bb13682f3285657291957ce37baad4/42.3601,-71.0589"; 
+  fetch(url).then(response => {
+response.json().then(json =>{
+    weather(json)
+    })
+  }) 
+};
 
-let weatherContainer = $("#weather-container");
+firsAjax()
 
-function getWeather(weather){
-  console.log(weather);
-  let weatherTemplate = " ";
-  let currently = weather.currently;
-  let temperature = currently.temperature;
-  let humidity = currently.humidity;
-  let pressure = currently.pressure;
-  let uvIndex = currently.uvIndex;
-  console.log(temperature);
-  console.log( humidity);
-  console.log(pressure);
-  console.log(uvIndex);
-  weatherTemplate += `
-  <div class="col m4">
-        <div class="col m12">
-          <div class="card">
-            <div class="card-image">
-              <img src="https://lintvwkbn.files.wordpress.com/2016/03/youngstown-ohio-weather-forecast-cloudy-7.jpg">
-            </div>
-            <div class="card-content">
-              <h4> Temperatura: ${temperature}</h4>
-              <h4> Humedad: ${humidity}</h4>
-              <h4> Presión: ${pressure}</h4>
-              <h4>Rayos Uv: ${uvIndex}</h4>
-            </div>
-          </div>
-          <a class="waves-effect waves-light btn modal-trigger" data-name="" href="#modal1">Clima de la semana</a>
-        </div>
-      </div>
-  `
-  weatherContainer.append(weatherTemplate);
+const weather = json =>{
+console.log(json);
+let weatherContainer = document.getElementById("weather-container");
+let weatherContainerWeek = document.getElementById("weather-container-week");
+
+let template =`
+<div>${json.currently.temperature}</div>
+<div>${json.currently.humidity}</div>
+<div>${json.currently.pressure}</div>
+<div>${json.currently.uvIndex}</div>
+<a class="waves-effect waves-light btn modal-trigger" data-name="" href="view.html">Clima de la semana</a>
+`;
+weatherContainer.innerHTML = template;
+
+let templateForecast = json.daily.data;
+console.log(templateForecast)
+templateForecast.forEach(day => {
+    console.log(day)
+
+
+   let currentDay = `
+    <div> Day: ${unixDateToCurrentDate(day.time)}</div>
+    <div> Icon: ${day.icon}</div>
+    <div> Hight Temperature:${day.temperatureHigh}</div>
+    <div> Min temperature: ${day.temperatureMin}</div>
+    <br>
+    `;
+    weatherContainerWeek.insertAdjacentHTML("beforeEnd",currentDay) 
+}); 
+};
+
+const  unixDateToCurrentDate = unixnumber =>{
+    //new - constructor
+console.log(new Date(unixnumber * 1000).toLocaleString("es-Mx", {weekday:"short"}));
+    return new Date(unixnumber * 1000).toLocaleString("es-Mx", {weekday:"long"});
 }
 
 
+
+/*
+//dark sky
 function weatherInfo(lat, lng){
-  console.log(lat, lng);
-  $.ajax({
-    url:`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/f3bb13682f3285657291957ce37baad4/${lat},${lng}`
-  }).done(getWeather)
-}
+    console.log(lat, lng);
+    $.ajax({
+      url:`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/f3bb13682f3285657291957ce37baad4/${lat},${lng}`
+    }).done(getWeather)
+  }
 
 
+// google 
 function getLocation(item){
-  console.log(item);
-  let results = item.results[0];
-  let location = results.geometry.location
-  let lat = location.lat;
-  let lng = location.lng;
-  console.log(location);
-  console.log(results, lat, lng);
-  weatherInfo(lat, lng)
-}
+    console.log(item);
+    let results = item.results[0];
+    let location = results.geometry.location
+    let lat = location.lat;
+    let lng = location.lng;
+    console.log(location);
+    console.log(results, lat, lng);
+    weatherInfo(lat, lng)
+  }
+  const form = $("#search-form");
+  form.submit(function(e){
+    e.preventDefault();
+    let cityId =$("#city");
+    let cityValue = cityId.val();
+    console.log(cityValue);
+    $.ajax({
+      url:`https://maps.googleapis.com/maps/api/geocode/json?address=${cityValue}&key=AIzaSyAgXOp-4JiH_7FCPWMgcBaPG2g9hg3WoSg`
+    }).done(getLocation)
+  })
+*/
 
-form.submit(function(e){
-  e.preventDefault();
-  let cityId =$("#city");
-  let cityValue = cityId.val();
-  console.log(cityValue);
-  $.ajax({
-    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${cityValue}&key=AIzaSyAgXOp-4JiH_7FCPWMgcBaPG2g9hg3WoSg`
-  }).done(getLocation)
-})
+    /*let day = document.getElementById("day");
+    let icon = document.getElementById("icon");
+    let higtTemperatureay = document.getElementById("higt-temperature");
+    let minTemperature = document.getElementById("min-temperature");
+    
+    day.innerHTML = unixDateToCurrentDate(day.time);
+    icon.innerHTML = day.icon;
+    higtTemperatureay,innerHTML = day.temperatureHigh
+    minTemperature.innerHTML = day.temperatureMin;*/
+
+//${unixDateToCurrentDate(day.time)}
